b0a130a26c73a81c55a19d259e2a7a52877aa98a
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-15 09:43:46 +0100

    added another function to exisiting function

--- controllers/company-controller.js
+++ controllers/company-controller.js
@@ -10,3 +10,21 @@ export const getAllCompanies = async (req, res, next) => {
     return res.status(500).json({message: "Internal server error"});
   }   
 };
+
+export const createCompany = async (req, res, next) => {
+    const {user_id, name, industry, description} = req.body;
+    
+    const company = new Company({
+        user_id,
+        name,
+        industry,
+        description
+    });
+
+    try {
+      await company.save();  
+    } catch (err) {
+        console.log(err);
+    }
+    return res.status(201).json({company});
+};
