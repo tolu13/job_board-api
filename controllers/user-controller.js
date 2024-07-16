a50f2f621c723c1d891b94dc93e9bcb4c117c3f5
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-16 08:34:25 +0100

    This function registers new users to the database

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,3 +17,50 @@ import mongoose from "mongoose";
     }
     return res.status(200).json({ users});
 };
+
+export const register = async (req, res, next) => {
+    const { name, email, password, user_type, companyName, industry, description } = req.body;
+  
+      let exisitingUser;
+      try {
+          exisitingUser = await User.findOne({ email });
+      } catch (err) {
+         return res.status(500).json({message: "database couldnt be accessed"});
+      }
+      if (exisitingUser) {
+          return res.status(400).json({message: "User Already Exists! Login Instead"});
+      }
+  
+      const hashedPassword = bcrypt.hashSync(password)
+  
+      const user = new User({
+          name,
+          email,
+          password: hashedPassword,
+          user_type
+      });
+          
+      try {
+          await user.save();
+      } catch (error) {
+          return res.status(400).json({message: "Check that your inputs are valid"});
+      }
+      
+      if (user_type === 'company') {
+  
+         const newCompany = new Company({
+              user_id: mongoose.user.id,
+              name: companyName,
+              industry,
+              description
+          }); 
+      try {
+          await newCompany.save();
+      }catch (err) {
+         return console.log(err);
+      }
+      return res.status(201).json({user})
+  }  
+  return res.status(201).json({message: "User created succesfully"});
+   
+  };
